5f1ac33fe6162c774428e78a5d9c1060e64c12aa
katakusu <katakusu1714@gmail.com>
2015-11-14 19:57:50 +0900

    各ページ先の処理を行う関数を追加

--- scraping.rb
+++ scraping.rb
@@ -3,6 +3,27 @@ require 'open-uri'
 # Nokogiriライブラリの読み込み
 require 'nokogiri'
 
+# 関数宣言
+
+### 各ページへのリンクを渡す ###
+def scrapePage(pagelink)
+  puts pagelink
+  charset = nil
+  html = open(pagelink) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+
+  doc.xpath('//div[@class="mdMTMWidget01Content01 MdCF"]/div[@class="mdMTMWidget01ItemTtl01"]'+
+    '/p[@class="mdMTMWidget01ItemTtl01View"]').each do |subtitle|
+      puts subtitle.text
+  end
+end
+
+
+
 # スクレイピング先のURL
 =begin
 for i in 1..3 do
@@ -34,7 +55,7 @@ doc.xpath('//li[@class="mdMTMTtlList01Item"]'+
     '/div[@class="mdMTMTtlList01Txt"]'+
     '/h3[@class="mdMTMTtlList01Ttl"]'+
     '/a/@href').each do |pagelinks|
-      puts pagelinks.text
+      scrapePage(pagelinks.text)
 end
 
 # タイトルを表示
