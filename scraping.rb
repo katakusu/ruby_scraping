4037bbbcbe4bba7b44cb9e53c03b8f9851660631
katakusu <katakusu1714@gmail.com>
2015-11-14 19:36:19 +0900

    NAVERにURLを設定

--- scraping.rb
+++ scraping.rb
@@ -4,7 +4,12 @@ require 'open-uri'
 require 'nokogiri'
 
 # スクレイピング先のURL
-url = 'http://www.yahoo.co.jp/'
+=begin
+for i in 1..3 do
+  puts i
+end
+=end
+url = 'http://matome.naver.jp/mymatome/a-r?page=1&order=U&type=C'
 
 charset = nil
 html = open(url) do |f|
@@ -15,5 +20,22 @@ end
 # htmlをパース(解析)してオブジェクトを生成
 doc = Nokogiri::HTML.parse(html, nil, charset)
 
+#====== 最大何ページあるか確認 ======
+maxpage = 1
+doc.xpath('//div[@class="MdPagination04"]/a').each do |pagenumber|
+  if maxpage < pagenumber.text.to_i
+    maxpage = pagenumber.text.to_i
+  end
+end
+puts maxpage
+
+#====== 1ページ内の各ページへのリンク =======
+doc.xpath('//li[@class="mdMTMTtlList01Item"]'+
+    '/div[@class="mdMTMTtlList01Txt"]'+
+    '/h3[@class="mdMTMTtlList01Ttl"]'+
+    '/a/@href').each do |pagelinks|
+      puts pagelinks.text
+end
+
 # タイトルを表示
-p doc.title
+# p doc.title
